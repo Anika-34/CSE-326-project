950e2573f53481cafbf10b8ba33cb34ce5e5c90e
Anika-34 <anikamorshed4434@gmail.com>
2026-03-11 13:55:40 +0600

    barch anika initial commit

--- client/src/components/CheckAvailability.jsx
+++ client/src/components/CheckAvailability.jsx
@@ -3,16 +3,20 @@ import { Fragment } from 'react'
 import { useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import Navbar from './Navbar';
+import { useState } from 'react';
 
 
 const CheckAvailability = () => {
     const { hotel_id } = useParams();
+    const [hotelDetails, setHotelDetails] = useState(null);
+
     const apiBaseUrl = process.env.REACT_APP_API_URL || '';
     const getAvailability = async (hotel_id) => {
         try {
             const response = await fetch(`${apiBaseUrl}/v1/hotels/details/${hotel_id}`);
             const data = await response.json();
             console.log(data);
+            setHotelDetails(data);
         } catch (err) {
             console.error(err.message);
         }
@@ -24,6 +28,13 @@ const CheckAvailability = () => {
     return <Fragment>
         <Navbar />
         <h1>Check Availability for Hotel ID: {hotel_id}</h1>
+        {hotelDetails ? (
+            <div>    
+                <p>{hotelDetails.description}</p>
+            </div>
+        ) : (
+            <p>Loading hotel details...</p>
+        )}
     </Fragment>
 }
 
